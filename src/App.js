abb8dd70ebba67a169a5feb734b18e7abc04417d
Joshua Coriell <joshuacoriell@Joshuas-MacBook-Pro.local>
2019-08-13 08:33:50 -0500

    updated headline

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
       <Container>
         <Image src="../images/STEM EDA Jumbotron-02.png" rounded fluid />
         {stemEdaThreads}
-        <p>hello</p>
+        <p>hey</p>
       </Container>
       
     )
